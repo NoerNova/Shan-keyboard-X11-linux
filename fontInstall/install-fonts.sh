cbbf179792c1432ebf46feac8bc5e9f4b4ee7e0a
NoerNova <noernova666@gmail.com>
2019-10-11 10:25:14 +0700

    add fonts && fontInstaller

---
+++ fontInstall/install-fonts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+fonts_dir=$(pwd)
+echo "Copy fonts from: $fonts_dir/fonts"
+
+find_command="find \"$fonts_dir/fonts\" \( -name '*.[o,t]tf' -or -name '*.pcf.gz' \) -type f -print0"
+
+system_font_dir="$HOME/.local/share/fonts"
+mkdir -p $system_font_dir
+
+
+# Copy all fonts to user fonts directory
+echo "Copying fonts..."
+# printing
+eval $find_command | xargs -0 -I %
+
+eval $find_command | xargs -0 -I % cp "%" "$system_font_dir/"
+
+  # Reset font cache
+ if command -v fc-cache @>/dev/null ; then
+        echo -e "\nResetting font cache, this may take a moment..."
+           fc-cache -f $system_font_dir
+           fi
+          
+          echo -e "\nAll fonts have been installed to $system_font_dir" ]]
